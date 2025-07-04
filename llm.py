a30e3d591af7b64f3d752f02d97e856a9b9bb270
Dtscience80 <101234762+Dtscience80@users.noreply.github.com>
2025-07-04 10:51:51 +0700

    Update llm.py

--- llm.py
+++ llm.py
@@ -5,7 +5,9 @@ st.set_page_config(page_title="🤖 Hugging Face Chat", layout="centered")
 st.title('🤖 Hugging Face LLM Chat')
 
 # Masukkan token Hugging Face di secrets.toml
-API_URL = "https://api-inference.huggingface.co/models/mistralai/Mistral-7B-Instruct-v0.2"
+#API_URL = "https://api-inference.huggingface.co/models/mistralai/Mistral-7B-Instruct-v0.2"
+API_URL = "https://api-inference.huggingface.co/models/gpt2"
+
 headers = {"Authorization": f"Bearer {st.secrets['HF_API_KEY']}"}
 st.write(st.secrets['HF_API_KEY'])
 
